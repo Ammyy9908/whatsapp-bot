c8764977d80efba3731150ff9eae0c8e25afd1c2
ammyy9908 <sb78639@gmail.com>
2022-04-01 15:22:33 +0530

    a simle bot response added

--- app/main.py
+++ app/main.py
@@ -13,9 +13,12 @@ def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
     msg = request.form.get('Body')
+    if msg == 'hi':
+        resp = MessagingResponse()
+        resp.message("Hello,How can i help you?")
 
-    # Create reply
-    resp = MessagingResponse()
-    resp.message("You said: {}".format(msg))
+    # # Create reply
+    # resp = MessagingResponse()
+    # resp.message("You said: {}".format(msg))
 
     return str(resp)
