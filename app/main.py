3197274b38d06d182a62630adb3ab34500630250
Sumit Kumar Bighaniya <sb78639@gmail.com>
2022-04-01 23:37:53 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,56 +1,24 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-import requests
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Phirlo Whatsapp Bot'
+    return '<h1>I want to Deploy Flask to Heroku</h1>'
 
 
 @app.route('/sms', methods=['POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
-    msg = request.form.get('Body').lower()
-    resp = MessagingResponse()
+    msg = request.form.get('Body')
     if msg == 'hi':
+        resp = MessagingResponse()
         resp.message("Hello,How can i help you?")
-    if 'quote' in msg:
-        # return a quote
-        r = requests.get('https://api.quotable.io/random')
-        if r.status_code == 200:
-            data = r.json()
-            quote = f'{data["content"]} ({data["author"]})'
-        else:
-            quote = 'I could not retrieve a quote at this time, sorry.'
-        resp.mesaage(quote)
-            
+
+    # Create reply
+    resp = MessagingResponse()
+    resp.message("You said: {}".format(msg))
 
     return str(resp)
-
-
-
-
-# incoming_msg = request.values.get('Body', '').lower()
-    
-#     msg = resp.message()
-#     responded = False
-#     if 'quote' in incoming_msg:
-#         # return a quote
-#         r = requests.get('https://api.quotable.io/random')
-#         if r.status_code == 200:
-#             data = r.json()
-#             quote = f'{data["content"]} ({data["author"]})'
-#         else:
-#             quote = 'I could not retrieve a quote at this time, sorry.'
-#         msg.body(quote)
-#         responded = True
-#     if 'cat' in incoming_msg:
-#         # return a cat pic
-#         msg.media('https://cataas.com/cat')
-#         responded = True
-#     if not responded:
-#         msg.body('I only know about famous quotes and cats, sorry!')
-#     return str(resp)
