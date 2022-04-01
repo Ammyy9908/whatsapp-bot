ce539a52cd64be82e31058b41ae546f9505b26c1
ammyy9908 <sb78639@gmail.com>
2022-04-01 16:14:55 +0530

    quotes added

--- app/main.py
+++ app/main.py
@@ -1,24 +1,34 @@
 from flask import Flask, request
+import requests
 from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>I want to Deploy Flask to Heroku</h1>'
+    return 'Phirlo Whatsapp Bot'
 
 
 @app.route('/sms', methods=['POST'])
 def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Fetch the message
-    msg = request.form.get('Body')
-    if msg == 'hi':
-        resp = MessagingResponse()
-        resp.message("Hello,How can i help you?")
-
-    # # Create reply
-    # resp = MessagingResponse()
-    # resp.message("You said: {}".format(msg))
-
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if 'quote' in incoming_msg:
+        # return a quote
+        r = requests.get('https://api.quotable.io/random')
+        if r.status_code == 200:
+            data = r.json()
+            quote = f'{data["content"]} ({data["author"]})'
+        else:
+            quote = 'I could not retrieve a quote at this time, sorry.'
+        msg.body(quote)
+        responded = True
+    if 'cat' in incoming_msg:
+        # return a cat pic
+        msg.media('https://cataas.com/cat')
+        responded = True
+    if not responded:
+        msg.body('I only know about famous quotes and cats, sorry!')
     return str(resp)
