186a1b19fad66049e0015b292ad2b6ef8ae55570
ammyy9908 <sb78639@gmail.com>
2022-04-01 16:29:25 +0530

    quotes added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
-import requests
 from twilio.twiml.messaging_response import MessagingResponse
+import requests
 app = Flask(__name__)
 
 
@@ -11,11 +11,13 @@ def index():
 
 @app.route('/sms', methods=['POST'])
 def sms_reply():
-    incoming_msg = request.values.get('Body', '').lower()
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body').lower()
     resp = MessagingResponse()
-    msg = resp.message()
-    responded = False
-    if 'quote' in incoming_msg:
+    if msg == 'hi':
+        resp.message("Hello,How can i help you?")
+    if 'quote' in msg:
         # return a quote
         r = requests.get('https://api.quotable.io/random')
         if r.status_code == 200:
@@ -23,12 +25,32 @@ def sms_reply():
             quote = f'{data["content"]} ({data["author"]})'
         else:
             quote = 'I could not retrieve a quote at this time, sorry.'
-        msg.body(quote)
-        responded = True
-    if 'cat' in incoming_msg:
-        # return a cat pic
-        msg.media('https://cataas.com/cat')
-        responded = True
-    if not responded:
-        msg.body('I only know about famous quotes and cats, sorry!')
+        resp.mesaage(quote)
+            
+
     return str(resp)
+
+
+
+
+# incoming_msg = request.values.get('Body', '').lower()
+    
+#     msg = resp.message()
+#     responded = False
+#     if 'quote' in incoming_msg:
+#         # return a quote
+#         r = requests.get('https://api.quotable.io/random')
+#         if r.status_code == 200:
+#             data = r.json()
+#             quote = f'{data["content"]} ({data["author"]})'
+#         else:
+#             quote = 'I could not retrieve a quote at this time, sorry.'
+#         msg.body(quote)
+#         responded = True
+#     if 'cat' in incoming_msg:
+#         # return a cat pic
+#         msg.media('https://cataas.com/cat')
+#         responded = True
+#     if not responded:
+#         msg.body('I only know about famous quotes and cats, sorry!')
+#     return str(resp)
