8f91beea1f0bee0f8d3f6f8d40aa589fbd1a1055
Sumit Kumar Bighaniya <sb78639@gmail.com>
2022-04-01 23:54:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/')
+def index():
+    return 'Phirlo Whatsapp Bot'
 
 
-@app.route("/sms", methods=['POST'])
+@app.route('/sms', methods=['POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
     msg = request.form.get('Body')
+    if msg == 'hi':
+        resp = MessagingResponse()
+        resp.message("Hello,How can i help you?")
 
-    # Create reply
-    resp = MessagingResponse()
-    resp.message("You said: {}".format(msg))
+    # # Create reply
+    # resp = MessagingResponse()
+    # resp.message("You said: {}".format(msg))
 
     return str(resp)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__=="__main__":
+  app.run()
