afcd5e79daf7e133872a0213dfd31a9659a6d650
ammyy9908 <sb78639@gmail.com>
2022-04-01 14:38:05 +0530

    twilo added

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
-from flask import Flask
- 
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
- 
+
+
 @app.route('/')
 def index():
-  return '<h1>I want to Deploy Flask to Heroku</h1>'
+    return '<h1>I want to Deploy Flask to Heroku</h1>'
+
+
+@app.route('/sms', methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
+
+    # Create reply
+    resp = MessagingResponse()
+    resp.message("You said: {}".format(msg))
+
+    return str(resp)
